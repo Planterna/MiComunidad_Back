1937bab67bbd4534d28a321e156fb7b15b5144d0
Edwin Salazar <edwinsalazarelpro@gmail.com>
2026-02-05 23:29:43 -0500

    backend historial uso dto y ajustes

---
+++ BackendCom/Models/HistorialUsoDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackendCom.Models
+{
+    public class HistorialUsoDto
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public int RecursoId { get; set; }
+        public string FechaUso { get; set; }
+        public string HoraInicio { get; set; }
+        public string HoraFin { get; set; }
+        public string Estado { get; set; }
+        public string? Notas { get; set; }
+        public bool Activo { get; set; }
+    }
+
+}
